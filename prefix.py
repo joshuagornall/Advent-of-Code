506a9c8a75c5ce4e553cd04c6fc697f311928fa9
Joshua Gornall <joshua.gornall@yahoo.co.uk>
2021-12-06 00:27:38 +0000

    Create prefix.py

---
+++ prefix.py
@@ -0,0 +1,21 @@
+"""
+Super speedy data reading and formatting prefix in the code. It seems all data is given in a format pretty similar.
+"""
+with open("input.txt", "r") as f:
+    src = data = f.read()
+
+if data and data[-1] == "\n":
+    data = data[:-1]
+
+py_input = input
+
+def input():
+    global data
+    if data:
+        try:
+            line, data = data.split("\n", 1)
+        except:
+            line = data
+            data = ""
+        return line
+    return py_input()
