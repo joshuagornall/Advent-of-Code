a70cacc15df9def1b41be6cc4ca2f3f299359058
Joshua Gornall <joshua.gornall@yahoo.co.uk>
2021-12-06 11:42:23 +0000

    daySix

---
+++ 2021/daySix/daySix.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+d = deque([0] * 9)
+
+for i in map(int, data.split(",")):
+    d[i] += 1
+
+for part in (80, 256 - 80):
+    for _ in range(part):
+        d.append(d.popleft())
+        d[6] += d[-1]
+    print(sum(d))
